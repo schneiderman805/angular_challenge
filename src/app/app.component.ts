3d81e4f07b64f50d28c0933100aa4dc45311661c
William Schneiderman <william.m.schneiderman@gmail.com>
2018-12-08 21:36:35 +0100

    played around with code but not sure if i understand it yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'new-app';
+  video = { 
+    title: 'Despacito',
+    views: 2,
+    liked: true
+  } 
+  handleClick(){
+    console.log('Someone clicked on button on the template :-)')
+  }
+  
 }
+
+export class SuggestionsComponent {
+  videos = [
+     {name: 'video one', liked: true},
+     {name: 'video two', liked: false}
+  ]
+  isShown: boolean = false
+  constructor() { }
+
+}
+
